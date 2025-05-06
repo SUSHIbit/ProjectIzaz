3755ad8b75c308dc84c9fd64d708544b40e1979c
Sushi Maru <paperquiffzz@gmail.com>
2025-05-06 11:52:02 +0800

    Updated folders

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -195,9 +195,8 @@
                         </button>
                         <a href="{{ route('home') }}" class="ml-2 flex items-center">
                             <svg class="h-8 w-8 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                             </svg>
-                            <span class="ml-2 text-xl font-semibold text-gray-900">Home Services</span>
                         </a>
                     </div>
                     <div class="flex items-center">
@@ -336,6 +335,7 @@
                             </div>
                         @endif
 
+                        <!-- THIS IS THE KEY PART - MAKE SURE TO RENDER THE SLOT HERE -->
                         {{ $slot }}
                     </div>
                 </div>
